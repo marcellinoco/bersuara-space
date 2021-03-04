db7f8c2cc2e084e89d5074f823ee0a62b51709dc
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-04 22:19:22 +0700

    Cloud build test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next Apps</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
