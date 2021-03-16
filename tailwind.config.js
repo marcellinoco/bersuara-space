8f85b0af170a158ca79dd0064e143e746f568b17
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-16 12:30:21 +0700

    Add default color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        purple: {
+          DEFAULT: '#C447FF',
+        },
+
+        gray: {
+          DEFAULT: '#777777',
+          dark: '#423F3F',
+          light: '#D9D9D9',
+        },
+
+        white: {
+          dark: '#F4F4F4',
+        },
+      },
+
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
