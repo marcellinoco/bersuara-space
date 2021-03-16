ca47fa6dbb9fbf9012a816a43fc8d1b125916fd4
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-16 12:30:44 +0700

    Created Navbar component

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>bersuara.space</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
