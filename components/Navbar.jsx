e03ac49dc87cb7523fba48e7e2129f3713cbd1e4
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-20 13:41:26 +0700

    Update navigation bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,31 +3,43 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row py-2 px-8">
-      {/* bersuara.space logo */}
-      <Link href="/">
-        <a className="text-lg font-medium">
-          <span className="text-purple">bersuara</span>.space
-        </a>
-      </Link>
-
+    <div className="flex flex-row justify-between px-8">
       {/* Navigation links */}
-      <div className="flex flex-row self-center text-sm font-normal select-none">
-        <Link href="">
+      <div className="flex flex-row self-center place-items-center my-2 text-sm font-normal select-none">
+        {/* bersuara.space logo */}
+        <Link href="/">
+          <a className="text-lg font-medium">
+            <span className="text-purple">bersuara</span>.space
+          </a>
+        </Link>
+
+        <Link href="/">
           <a className="ml-16">Support</a>
         </Link>
 
-        <Link href="">
+        <Link href="/">
           <a className="ml-16">Forums</a>
         </Link>
 
-        <Link href="">
+        <Link href="/">
           <a className="ml-16">News</a>
         </Link>
 
-        <Link href="">
+        <Link href="/">
           <a className="ml-16">Contacts</a>
         </Link>
+      </div>
+
+      <div className="flex flex-row self-stretch place-items-center text-sm font-medium">
+        <Link href="/">
+          <a className="mr-10">sign in</a>
+        </Link>
+
+        <div className="flex self-stretch bg-purple">
+          <Link href="/">
+            <a className="self-center mx-6 text-white">sign up</a>
+          </Link>
+        </div>
       </div>
     </div>
   )
