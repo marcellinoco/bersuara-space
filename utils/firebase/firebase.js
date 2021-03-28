3b4aebd0c42a4806783693bcd8ea923573cb6d0d
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-29 00:19:01 +0700

    Update to firebase-admin #2

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase-admin'
 
-var serviceAccount = require('./firebase-service-account-key.json')
-
 /**
  * Initialize Firebase (if not yet initialized), and return its instance.
  * @returns firebase instance
  */
 function getFirebase() {
   if (!firebase.apps.length) firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount),
+    credential: firebase.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
     databaseURL: "https://bersuara-space-default-rtdb.firebaseio.com",
   })
   return firebase
