4c3e09a720d3596dfb97d1a3605b5d9e96d44ef5
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-28 23:10:16 +0700

    Populate contacts page with data from Firebase Firestore

---
+++ components/contacts/ContactsRegion.jsx
@@ -0,0 +1,18 @@
+import ContactDetails from './ContactDetails'
+
+export default function ContactsRegion({ regionName, contactsList, className }) {
+  return (
+    <div className={`${className} flex flex-col items-center`}>
+      <h2 className="text-lg font-bold">
+        {regionName}
+      </h2>
+
+      { contactsList.map((contactDetails) => (
+        <ContactDetails 
+          contactDetails={contactDetails}
+          className="mt-4"
+        />
+      )) }
+    </div>
+  )
+}
