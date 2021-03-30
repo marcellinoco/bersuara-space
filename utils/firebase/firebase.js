a0e7b0636fa4b822bf67c8eb5f60549fb86aef65
MarcellinoCO <marcellinocovara@gmail.com>
2021-03-30 18:01:44 +0700

    Update Firebase configuration

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -8,7 +8,7 @@ function getFirebase() {
   if (!firebase.apps.length) firebase.initializeApp({
     credential: firebase.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: "https://bersuara-space-default-rtdb.firebaseio.com",
